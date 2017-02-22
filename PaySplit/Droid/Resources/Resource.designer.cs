08d1e6e4b644d2d0564b967007f4c1f34821b253
Yash Patel <ydpatel@uwaterloo.ca>
2017-02-21 19:29:57 -0500

    Added partial android views

--- PaySplit/Droid/Resources/Resource.designer.cs
+++ PaySplit/Droid/Resources/Resource.designer.cs
@@ -44,20 +44,35 @@ namespace PaySplit.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int AddEntry = 2131034112;
-			
-			// aapt resource value: 0x7f050001
-			public const int Viewbtn = 2131034113;
+			// aapt resource value: 0x7f050002
+			public const int AddEntry = 2131034114;
 			
 			// aapt resource value: 0x7f050003
-			public const int imageView = 2131034115;
+			public const int Viewbtn = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int addButton = 2131034118;
+			
+			// aapt resource value: 0x7f050005
+			public const int imageView = 2131034117;
+			
+			// aapt resource value: 0x7f050001
+			public const int imageView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050008
+			public const int listView = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int myButton = 2131034121;
 			
 			// aapt resource value: 0x7f050004
-			public const int myButton = 2131034116;
+			public const int picture = 2131034116;
 			
-			// aapt resource value: 0x7f050002
-			public const int picture = 2131034114;
+			// aapt resource value: 0x7f050007
+			public const int searchBar = 2131034119;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
 			
 			static Id()
 			{
@@ -73,10 +88,16 @@ namespace PaySplit.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int CreateEntry = 2130903040;
+			public const int CreateBillEntry = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int CreateEntry = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int DisplayBills = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int Main = 2130903043;
 			
 			static Layout()
 			{
