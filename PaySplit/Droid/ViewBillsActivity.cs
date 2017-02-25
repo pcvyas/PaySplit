9634c5bbb60365c942c62914c98b8101aedfe4ed
JATHUSAN THIRUCHELVANATHAN <j3thiruc@uwaterloo.ca>
2017-02-24 19:39:32 -0500

    Fix View Bills Activity Label

--- PaySplit/Droid/ViewBillsActivity.cs
+++ PaySplit/Droid/ViewBillsActivity.cs
@@ -12,7 +12,7 @@ using Android.Widget;
 
 namespace PaySplit.Droid
 {
-    [Activity(Label = "View Bill", MainLauncher = false, Icon = "@mipmap/new_icon", Theme = "@android:style/Theme.Material.Light")]
+    [Activity(Label = "View Bills", MainLauncher = false, Icon = "@mipmap/new_icon", Theme = "@android:style/Theme.Material.Light")]
     public class ViewBillsActivity : Activity
     {
         private List<Bill> bills;
