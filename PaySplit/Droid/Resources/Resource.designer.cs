7764638207719779ecdeec4f7c44580a17b36986
JATHUSAN THIRUCHELVANATHAN <j3thiruc@uwaterloo.ca>
2017-03-13 13:18:17 -0400

    Remove unused image

--- PaySplit/Droid/Resources/Resource.designer.cs
+++ PaySplit/Droid/Resources/Resource.designer.cs
@@ -64,16 +64,13 @@ namespace PaySplit.Droid
 			public const int addbill = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int back2 = 2130837505;
+			public const int big_card = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int big_card = 2130837506;
+			public const int rectangle = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int rectangle = 2130837507;
-			
-			// aapt resource value: 0x7f020004
-			public const int Rounded = 2130837508;
+			public const int Rounded = 2130837507;
 			
 			static Drawable()
 			{
