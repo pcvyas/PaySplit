a8a5d66974fcef59a259dc8049617f15065defe8
JATHUSAN THIRUCHELVANATHAN <j3thiruc@uwaterloo.ca>
2017-03-21 12:47:33 -0400

    Editable owners in details activity

--- PaySplit/Droid/Activities/BillDetailsActivity.cs
+++ PaySplit/Droid/Activities/BillDetailsActivity.cs
@@ -25,6 +25,7 @@ namespace PaySplit.Droid
 		private TextView date;
 		private TextView category;
 		private TextView desc;
+		private TextView owner;
 		private ImageView image;
 		private TextView updated;
 		private Button editButton;
@@ -35,6 +36,7 @@ namespace PaySplit.Droid
 		private TextView date_edit;
 		private Spinner category_edit;
 		private EditText desc_edit;
+		private Spinner owner_edit;
 		private Button saveButton;
 
 		// Instantiate ViewSwitchers used for switching to edit mode
@@ -44,10 +46,15 @@ namespace PaySplit.Droid
 		private ViewSwitcher descSwitcher;
 		private ViewSwitcher buttonSwitcher;
 		private ViewSwitcher dateSwitcher;
+		private ViewSwitcher ownerSwitcher;
 
 		ArrayAdapter<String> adapter;
+		ContactsSuggestionArrayAdapter mContactsAdapter;
+
+		List<Contact> mContacts;
 
 		int categoryIndex = 0;
+		int ownerIndex = 0;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -72,13 +79,15 @@ namespace PaySplit.Droid
             date = FindViewById<TextView>(Resource.Id.Details_Date);
             category = FindViewById<TextView>(Resource.Id.Details_BillCategory);
             desc = FindViewById<TextView>(Resource.Id.Details_BillDesc);
-            image = FindViewById<ImageView>(Resource.Id.Details_imageView);
+			owner  = FindViewById<TextView>(Resource.Id.Details_BillOwner);
+			image = FindViewById<ImageView>(Resource.Id.Details_imageView);
             updated = FindViewById<TextView>(Resource.Id.Details_Updated);
             editButton = FindViewById<Button>(Resource.Id.Details_EditButton);
             name_edit = FindViewById<EditText>(Resource.Id.Details_BillName_Edit);
             amount_edit = FindViewById<EditText>(Resource.Id.Details_BillAmount_Edit);
 			date_edit = FindViewById<TextView>(Resource.Id.Details_Date_Edit);
             category_edit = FindViewById<Spinner>(Resource.Id.Details_BillCategory_Edit);
+			owner_edit = FindViewById<Spinner>(Resource.Id.Details_BillOwner_Edit);
             desc_edit = FindViewById<EditText>(Resource.Id.Details_BillDesc_Edit);
             saveButton = FindViewById<Button>(Resource.Id.Details_SaveButton);
 			deleteButton = FindViewById<Button>(Resource.Id.deleteBillButton);
@@ -87,16 +96,10 @@ namespace PaySplit.Droid
 			nameSwitcher = FindViewById<ViewSwitcher>(Resource.Id.Details_name_switcher);
 			amountSwitcher = FindViewById<ViewSwitcher>(Resource.Id.Details_amount_switcher);
 			categorySwitcher = FindViewById<ViewSwitcher>(Resource.Id.Details_category_switcher);
+			ownerSwitcher = FindViewById<ViewSwitcher>(Resource.Id.Details_OwnerSwitcher);
 			descSwitcher = FindViewById<ViewSwitcher>(Resource.Id.Details_desc_switcher);
 			buttonSwitcher = FindViewById<ViewSwitcher>(Resource.Id.Details_button_switcher);
 			dateSwitcher = FindViewById<ViewSwitcher>(Resource.Id.Details_date_switcher);
-
-			// Populate the views
-			name.Text = mBill.Name;
-			amount.Text = "$" + String.Format("{0:0.00}", mBill.Amount); // rounds to 2 decimal places
-			date.Text = mBill.Date.ToString("MMMM dd, yyyy");
-			category.Text = mBill.Category;
-			desc.Text = mBill.Description;
 			    
             // Initialize the Categories Spinner
             String[] categories = Resources.GetStringArray(Resource.Array.categories_array);
@@ -112,12 +115,35 @@ namespace PaySplit.Droid
             adapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, categories);
             category_edit.Adapter = adapter;
 
+			mContacts = mDBS.GetAllContacts();
+			ownerIndex = 0;
+			for (int i = 0; i < mContacts.Count; i++)
+			{
+				if (mContacts[i].UID == mBill.OwnerUID)
+				{
+					ownerIndex = i;
+					break;
+				}
+			}
+			mContactsAdapter = new ContactsSuggestionArrayAdapter(this, mContacts);
+			owner_edit.Adapter = mContactsAdapter;
+
 			LoadImageForBill();
 			updateLastUpdatedTimestampText();
 
 			editButton.Click += Edit_Click;
 			saveButton.Click += Save_Click;
 			deleteButton.Click += Delete_Click;
+
+			Contact c = mDBS.getContactByUID(mBill.OwnerUID);
+
+			// Populate the views
+			name.Text = mBill.Name;
+			amount.Text = "$" + String.Format("{0:0.00}", mBill.Amount); // rounds to 2 decimal places
+			date.Text = mBill.Date.ToString("MMMM dd, yyyy");
+			category.Text = mBill.Category;
+			desc.Text = mBill.Description;
+			owner.Text = mContacts[ownerIndex].FullName;
 
 			this.ActionBar.SetDisplayHomeAsUpEnabled(true);
         }
@@ -173,18 +199,19 @@ namespace PaySplit.Droid
 			nameSwitcher.ShowNext();
 			amountSwitcher.ShowNext();
 			categorySwitcher.ShowNext();
+			ownerSwitcher.ShowNext();
 			descSwitcher.ShowNext();
 			buttonSwitcher.ShowNext();
 			dateSwitcher.ShowNext();
 
 			date_edit.Click += Date_Click;
-			date_edit.Background = GetDrawable(Resource.Drawable.big_card);
 
 			name_edit.SetText(name.Text, TextView.BufferType.Editable);
 			amount_edit.SetText(amount.Text.Substring(1), TextView.BufferType.Editable); // dangerous string concat to get rid of dollar sign, error check later
 			date_edit.SetText(date.Text, TextView.BufferType.Editable);
 			category_edit.SetSelection(categoryIndex);
 			desc_edit.SetText(desc.Text, TextView.BufferType.Editable);
+			owner_edit.SetSelection(ownerIndex);
 		}
 
 		void Save_Click(object sender, EventArgs e)
@@ -192,6 +219,7 @@ namespace PaySplit.Droid
             try
             {
                 string billCat = adapter.GetItem(category_edit.SelectedItemPosition);
+				Contact c = mContacts[owner_edit.SelectedItemPosition];
                 double billAmount = Double.Parse(amount_edit.Text);
                 ISharedPreferences sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(this);
                 string catLimit = sharedPreferences.GetString(billCat, "0");
@@ -220,6 +248,7 @@ namespace PaySplit.Droid
                 nameSwitcher.ShowPrevious();
                 amountSwitcher.ShowPrevious();
                 categorySwitcher.ShowPrevious();
+				ownerSwitcher.ShowPrevious();
                 descSwitcher.ShowPrevious();
                 buttonSwitcher.ShowPrevious();
 
@@ -228,6 +257,7 @@ namespace PaySplit.Droid
                 mBill.Date = Convert.ToDateTime(date_edit.Text); // date format check here
                 mBill.Category = billCat;
                 mBill.Description = desc_edit.Text;
+				mBill.OwnerUID = c.UID;
 
                 mDBS.SaveBillEntry(mBill.Id, mBill);
 
@@ -236,6 +266,7 @@ namespace PaySplit.Droid
                 date.Text = date_edit.Text;
                 category.Text = adapter.GetItem(category_edit.SelectedItemPosition);
                 desc.Text = desc_edit.Text;
+				owner.Text = mContacts[owner_edit.SelectedItemPosition].FullName;
 
                 Toast.MakeText(this, "Update Successful", ToastLength.Short).Show();
             }
